21383ecbda9bcd313f76501e326d84abe3c7433d
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2026-09-10 14:34:28 -0300

    Adiciona domínio do Módulo de Pedidos

--- src/Infraestrutura/Compartilhado/Orm/Migrations/20260902171220_Seed_ClienteRole.cs
+++ src/Infraestrutura/Compartilhado/Orm/Migrations/20260902171220_Seed_ClienteRole.cs
@@ -2,27 +2,26 @@
 
 #nullable disable
 
-namespace DeliveryApp.Infraestrutura.Compartilhado.Orm.Migrations
+namespace DeliveryApp.Infraestrutura.Compartilhado.Orm.Migrations;
+
+/// <inheritdoc />
+public partial class Seed_ClienteRole : Migration
 {
     /// <inheritdoc />
-    public partial class Seed_ClienteRole : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.InsertData(
-                table: "AspNetRoles",
-                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
-                values: new object[] { new Guid("01a058f4-a048-79a3-b1a6-0f01d629a126"), "01a058f7-9492-73bc-8e4b-934c53594ed6", "Cliente", "CLIENTE" });
-        }
+        migrationBuilder.InsertData(
+            table: "AspNetRoles",
+            columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+            values: new object[] { new Guid("01a058f4-a048-79a3-b1a6-0f01d629a126"), "01a058f7-9492-73bc-8e4b-934c53594ed6", "Cliente", "CLIENTE" });
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DeleteData(
-                table: "AspNetRoles",
-                keyColumn: "Id",
-                keyValue: new Guid("01a058f4-a048-79a3-b1a6-0f01d629a126"));
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DeleteData(
+            table: "AspNetRoles",
+            keyColumn: "Id",
+            keyValue: new Guid("01a058f4-a048-79a3-b1a6-0f01d629a126"));
     }
 }
