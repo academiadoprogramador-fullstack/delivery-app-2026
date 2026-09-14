4f91a5d6ddd3b0a96a27ebb9af28bb5b79c0bff2
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2026-09-14 15:24:25 -0300

    Adiciona Migration para TBTransicoesStatusPedido

--- src/Infraestrutura/Compartilhado/Orm/Config/PedidoConfiguration.cs
+++ src/Infraestrutura/Compartilhado/Orm/Config/PedidoConfiguration.cs
@@ -50,6 +50,11 @@ public sealed class PedidoConfiguration : IEntityTypeConfiguration<Pedido>
             .WithOne()
             .HasForeignKey(i => i.PedidoId)
             .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasMany(p => p.Historico)
+            .WithOne()
+            .HasForeignKey(h => h.PedidoId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
 
@@ -100,5 +105,35 @@ public sealed class ComplementoItemPedidoConfiguration : IEntityTypeConfiguratio
         builder.Property(c => c.PrecoAdicional)
             .HasPrecision(10, 2)
             .IsRequired();
+    }
+}
+
+public sealed class TransicaoStatusPedidoConfiguration : IEntityTypeConfiguration<TransicaoStatusPedido>
+{
+    public void Configure(EntityTypeBuilder<TransicaoStatusPedido> builder)
+    {
+        builder.ToTable("TBTransicoesStatusPedido");
+
+        builder.HasKey(t => t.Id);
+        builder.Property(t => t.Id).ValueGeneratedNever();
+
+        builder.Property(t => t.StatusAnterior)
+            .HasConversion<string>()
+            .HasMaxLength(30);
+
+        builder.Property(t => t.StatusAtual)
+            .HasConversion<string>()
+            .HasMaxLength(30)
+            .IsRequired();
+
+        builder.Property(t => t.TipoUsuario)
+            .HasConversion<string>()
+            .HasMaxLength(30)
+            .IsRequired();
+
+        builder.Property(t => t.Motivo)
+            .HasMaxLength(TransicaoStatusPedido.TamanhoMaximoMotivo);
+
+        builder.HasIndex(t => new { t.PedidoId, t.OcorridaEmUtc });
     }
 }
